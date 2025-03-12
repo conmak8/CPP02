a1af8b7f429d2fed2822c718fb53a5f868fe9210
Constantinos Makariou <cmakario@student.42heilbronn.de>
2025-03-12 13:05:07 +0100

    02: copied previous files and main from pdf

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,13 +6,14 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 22:02:09 by cmakario          #+#    #+#             */
-/*   Updated: 2025/03/12 11:08:42 by cmakario         ###   ########.fr       */
+/*   Updated: 2025/03/12 12:39:26 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main() {
+int main()
+{
 	
 	Fixed a;
 	// a.setRawBits(1);
