b31161d15c7133952bf0a841ee2e7cf66f54fa2d
Constantinos Makariou <cmakario@student.42heilbronn.de>
2025-03-14 18:54:43 +0100

    03: necessary files added

--- ex03/include/Point.hpp
+++ ex03/include/Point.hpp
@@ -1,32 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Point.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/12 12:40:29 by cmakario          #+#    #+#             */
-/*   Updated: 2025/03/14 17:58:54 by cmakario         ###   ########.fr       */
+/*   Created: 2025/03/14 18:52:50 by cmakario          #+#    #+#             */
+/*   Updated: 2025/03/14 18:53:00 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/Fixed.hpp"
-
-
-int main(void)
-{
-	Fixed		a;
-	Fixed const b(Fixed(5.05f) * Fixed(2));
-
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	
-	std::cout << b << std::endl;
-
-	std::cout << Fixed::max(a, b) << std::endl;
-	
-	return 0;
-}
