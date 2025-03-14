75b273220829966b96aa2fb13e68a4820ce11db7
Constantinos Makariou <cmakario@student.42heilbronn.de>
2025-03-14 19:21:38 +0100

    03: fixed point header file

--- ex03/include/Point.hpp
+++ ex03/include/Point.hpp
@@ -6,7 +6,26 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 18:52:50 by cmakario          #+#    #+#             */
-/*   Updated: 2025/03/14 18:53:00 by cmakario         ###   ########.fr       */
+/*   Updated: 2025/03/14 19:19:27 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef POINT_HPP
+# define POINT_HPP
+
+#include "Fixed.hpp"
+class Point
+{
+private:
+		Fixed const	x;
+		Fixed const	y;
+public:
+		Point(); //Constructor
+		Point(const float x, const float y);
+		Point(const Point &copy);
+		Point &operator= (const Point &copy);
+		~Point();
+
+};
+
+#endif // !POINT_HPP
--- ex03/src/Point.cpp
+++ ex03/src/Point.cpp
@@ -6,7 +6,15 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 18:52:52 by cmakario          #+#    #+#             */
-/*   Updated: 2025/03/14 18:53:01 by cmakario         ###   ########.fr       */
+/*   Updated: 2025/03/14 19:20:23 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../include/Fixed.hpp"
+#include "../include/Point.hpp"
+
+
+Point::Point() : x(0), y(0) {}
+
+Point::Point(const float x, const float y) : x(0), y(0) {}
 
